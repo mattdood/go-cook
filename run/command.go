9d94da0955bb97df24cdc599fe0d307bf961845a
mattdood <mawimberly@csumb.edu>
2022-12-20 19:37:17 -0800

    Run: Function stubs for git commands

--- run/command.go
+++ run/command.go
@@ -9,3 +9,24 @@ func Create(title string, template string) {
     fmt.Println("Hello from the command.go file: ", title)
 }
 
+// TODO:
+// - Function stub placeholder
+func Git(subcommand string) {
+    fmt.Println("Hello from the git command: ", subcommand)
+}
+
+func Add(files []string) {
+    fmt.Println("Hello from the (git) add command: ", files)
+}
+
+func Init() {
+    fmt.Println("Hello from the init command")
+}
+
+func Push() {
+    fmt.Println("Hello from the push command")
+}
+
+func Pull() {
+    fmt.Println("Hello from the pull command")
+}
