51baa7b631cd9722f82bc2999cd7a0b72124b6e8
mattdood <mawimberly@csumb.edu>
2022-12-21 17:30:49 -0800

    CLI: Help text properly displays with help flag on 0 arg executions

--- cli/run.go
+++ cli/run.go
@@ -8,7 +8,7 @@ const usage = `
 go-cook [OPTION] [args...]
 
 Options:
-    -h, --help display this help
+    -h, -help, --help display this help
 
 Executes various commands to manipulate cooking templates defined
 in YAML format.
@@ -27,8 +27,20 @@ type CommandArgs struct {
 // appropriate command, return exit call
 // of the given command
 func Run(args []string) int {
-    if len(args) <= 1 {
-        fmt.Println(usage)
+
+    // args are:
+    // [/tmp/go-build-dir <cmd flag>]
+    if len(args) > 1{
+        fmt.Println(args)
+        for _, arg := range args {
+            fmt.Println(arg)
+            if arg == "-h" || arg == "-help" || arg == "--help" {
+                fmt.Println(usage)
+                return 1
+            }
+        }
+    } else {
+        fmt.Println("Unknown command. Use -h, -help, or --help to display help")
         return 1
     }
 
