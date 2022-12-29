7f8bb274394f6e5940daef71b0e81c4e6ef31284
mattdood <mawimberly@csumb.edu>
2022-12-28 19:06:18 -0800

    Run: Created Create() command that makes a file in the specified format, passes TemplateData to it, then opens in vim

--- run/command.go
+++ run/command.go
@@ -7,6 +7,8 @@ import (
     "os/exec"
     "path"
     "strings"
+    "text/template"
+    "time"
 )
 
 var (
@@ -26,10 +28,93 @@ func getUserHome() string {
     return homedir
 }
 
-// TODO:
-//   * timestamp for template
-func Create(title string, category string, tags []string, template string) {
-    fmt.Println("Hello from the command.go file: ", title)
+type TemplateData struct {
+    Title string
+    Category string
+    Tags []string
+    Timestamp string
+}
+
+func NewTemplateData(title string, category string, tags []string, timestamp string) *TemplateData {
+    return &TemplateData{
+        Title: title,
+        Category: category,
+        Tags: tags,
+        Timestamp: timestamp,
+    }
+}
+
+// Create a new file based on the template data input via the CLI
+func Create(title string, category string, tags []string, templateType string) int {
+    // Find template file to load
+    var tmpl *template.Template
+    var err error
+    switch {
+    case templateType == "recipe":
+        tmpl, err = template.ParseFiles("run/recipe.tmpl")
+        if err != nil {
+            fmt.Println("Error parsing `recipe.tmpl` template file: ", err)
+            return 1
+        }
+    case templateType == "tip":
+        tmpl, err = template.ParseFiles("run/tip.tmpl")
+        if err != nil {
+            fmt.Println("Error parsing `tip.tmpl` template file: ", err)
+            return 1
+        }
+    }
+
+    // Path interpolation
+    var timestamp time.Time
+    timestamp = time.Now()
+
+    var filename string
+    filename = fmt.Sprintf("%s-%s.yml", strings.Replace(title, " ", "-", -1), timestamp.Format("20060102150405"))
+
+    var outputPath string
+    outputPath = path.Join(
+        OutputDirectory,
+        templateType,
+        fmt.Sprintf("%d/%02d/%02d/", timestamp.Year(), timestamp.Month(), timestamp.Day()),
+        timestamp.Format("20060102150405"),
+        filename,
+    )
+
+    // Create all directories to the file
+    if err := os.MkdirAll(path.Dir(outputPath), 0775); err != nil {
+        fmt.Println(err)
+        return 2
+    }
+
+    // Create file
+    file, err := os.Create(outputPath)
+    if err != nil {
+        fmt.Println(err)
+        return 2
+    }
+
+    // Load template data
+    err = tmpl.Execute(
+        file,
+        NewTemplateData(title, category, tags, timestamp.Format("20060102150405")),
+    )
+    if err != nil {
+        fmt.Println("Error in template execution: ", err)
+        return 2
+    }
+    file.Close()
+
+    // Open in vim
+    cmd := exec.Command("vim", outputPath)
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    err = cmd.Run()
+    if err != nil {
+        fmt.Println("Error in vim execution: ", err)
+        return 2
+    }
+
+    return 0
 }
 
 // Git command wrapper for `git add`
